d92f53fdd5f37d58cc7544db3b6ddc7bb8456285
Julie Vaccalluzzo <mail@juliestephanie.com>
2020-04-30 21:18:50 +0100

    origional solution

--- index.js
+++ index.js
@@ -1,86 +1,60 @@
+function copyToCanvas(image) {
+	const can = document.createElement('canvas');
 
+	can.width = image.naturalWidth || image.width;
+	can.height = image.naturalHeight || image.height;
+	can.ctx = can.getContext('2d');
+	can.ctx.drawImage(image, 0, 0);
+	return can;
+}
+
+const channels = {
+	red: '#F00',
+	green: '#0F0',
+	blue: '#00F',
+};
+
+function getChannel(channelName, image) {
+	const copy = copyToCanvas(image);
+	const ctx = copy.ctx;
+	ctx.fillStyle = channels[channelName];
+	ctx.globalComposietOperation = 'multiply';
+	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	ctx.globalComposietOperation = 'destination-in';
+	ctx.drawImage(image, 0, 0);
+	ctx.globalComposietOperation = 'source-over';
+	return copy;
+}
+
+function seperateRGB(image) {
+	return {
+		red: getChannel('red', image),
+		green: getChannel('green', image),
+		blue: getChannel('blue', image),
+	};
+}
+
+function createCanvas(w, h) {
+	const can = document.createElement('canvas');
+	can.width = w;
+	can.height = h;
+	can.ctx = can.getContext('2d');
+	return can;
+}
 
 const image = document.querySelector('#source-image');
+const output = document.querySelector('.output');
 
-const canvas = document.querySelector('.canvas');
-let ctx = canvas.getContext('2d');
+const RGB = seperateRGB(image);
+const recombined = createCanvas(RGB.red.width, RGB.red.height);
+const ctx = recombined.ctx;
 
+ctx.drawImage(RGB.red, -2, -2);
+ctx.globalCompositeOperation = 'lighter';
+ctx.drawImage(RGB.green, 0, 0);
+ctx.drawImage(RGB.blue, 2, 2);
+ctx.globalCompositeOperation = 'destination-in';
+ctx.drawImage(image, 0, 0);
+ctx.globalCompositeOperation = 'source-over';
 
- const drawImage = (image) => {
-  // Set the canvas the same width and height of the image
-  console.info(image.clientHeight);
-  console.info(image.clientWidth);
-  canvas.width = image.width;
-  canvas.height = image.height;
-  // ctx.filter = 'opacity(50%)';
-  ctx.globalCompositeOperation = 'multiply';
-  ctx.drawImage(image, 0, 0);
-}
-
-drawImage(image);
-
-// Or
-  // var image = new Image();
-  // image.onload = function () {
-  //    drawImage(image);
-  // }
-  // image.src = 'image.jpg';
-
-// let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-image.data = new Uint8ClampedArray(); // WRONG
-image.data[1] = 0; // CORRECT
-
-
-const imageDataRed = ctx.getImageData(0, 0, canvas.width, canvas.height);
-const imageDataGreen = ctx.getImageData(0, 0, canvas.width, canvas.height);
-const imageDataBlue = ctx.getImageData(0, 0, canvas.width, canvas.height);
-console.info(imageDataRed);
-const redAlphaChanel = (data) =>   {
-  console.info(data);
-  data.forEach((item, i)=>{
-    i < 100 ? console.info(i+= 4) : null;
-  });
-  for (var i = 0; i < data.length; i+= 4) {
-    i < 100 ? console.info(i+= 4) : null;
-    data[i] = data[i]; // Red
-    data[i+1] = 0; // Green
-    data[i+2] = 0; // Blue
-  }
-}
-const greenAlphaChanel = (data) =>   {
-  console.info(data);
-  data.forEach((item, i)=>{
-    i < 100 ? console.info(i+= 4) : null;
-  });
-  for (var i = 0; i < data.length; i+= 4) {
-    i < 100 ? console.info(i+= 4) : null;
-    data[i] = 0; // Red
-    data[i+1] = data[i + 1]; // Green
-    data[i+2] = 0; // Blue
-  }
-}
-const blueAlphaChanel = (data) =>   {
-  console.info(data);
-  data.forEach((item, i)=>{
-    i < 100 ? console.info(i+= 4) : null;
-  });
-  for (var i = 0; i < data.length; i+= 4) {
-    i < 100 ? console.info(i+= 4) : null;
-    data[i] = 0; //  Red
-    data[i+1] = 0//  Green
-    data[i+2] = data[i + 2]; //  Blue
-  }
-}
-
-redAlphaChanel(imageDataRed.data);
-greenAlphaChanel(imageDataGreen.data);
-blueAlphaChanel(imageDataBlue.data);
-
-
-  // Update the canvas with the new data
-
-
-  ctx.putImageData(imageDataRed, 0, 0);
-  ctx.putImageData(imageDataGreen, 20, 20);
-  ctx.putImageData(imageDataBlue, 40, 40);
+output.append(recombined);

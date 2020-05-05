42e3d1c4664def5b9e6e2b2f9025d22be76f0ce3
Julie Vaccalluzzo <mail@juliestephanie.com>
2020-05-05 18:47:57 +0100

    restore point

--- index.js
+++ index.js
@@ -14,15 +14,90 @@ const channels = {
 	blue: '#00F',
 };
 
+const returnChannel = (channelName, data) => {
+	console.log(typeof channelName);
+	// console.info(channelName, data);
+
+	/* 	for (var i = 0; i < data.length; i += 4) {
+		data[i] = data[i]; // Red
+		data[i + 1] = 0; // Green
+		data[i + 2] = 0; // Blue
+	} */
+	/* 	function isBigEnough(element, i, data) {
+		i < 48 && i % 4 === 0 ? console.info(element) : null;
+		return element >= 10;
+	} */
+
+	let arr = [19, 29, 39, 255, 18, 28, 38, 255, 15, 25, 35, 255, 14, 24, 34, 255, 13, 23, 33, 255, 16, 26, 36, 255, 17, 27, 37, 255, 15, 25, 35, 255, 16, 26, 36, 255, 16, 26, 36, 255, 16, 26, 36, 255, 16, 26, 36, 255, 15];
+
+	console.log(`arr: ${arr}`);
+	let newArr = arr.map((element, index) => {
+		// createLayer.red(element);
+		//red
+		/* 		const renderRed = channelName === 'red' && (index % 4 === 1 || index % 4 === 2);
+		const renderGreen = channelName === 'green' && (index % 4 === 0 || index % 4 === 2);
+		const renderBlue = channelName === 'blue' && (index % 4 === 0 || index % 4 === 1);
+		console.log(renderRed);
+		// console.log(`${channelName} ${element} red: ${renderRed}, green:${renderGreen}, blue: ${renderBlue}`);
+		// index % 4 === 0 || index % 4 === 3 ? null : (element = 0);
+		switch ((element, index)) {
+		case 'renderRed':
+			return (element = 0);
+		case 'renderGreen':
+			console.log(`${channelName} element`);
+			return (element = 0);
+		case 'renderBlue':
+			console.log(`${channelName} element`);
+			return (element = 0);
+
+		default:
+			return element;
+						// break;
+		} */
+
+		if (channelName === 'red' && (index % 4 === 1 || index % 4 === 2)) {
+			return (element = 0);
+		} else if (channelName === 'green' && (index % 4 === 0 || index % 4 === 2)) {
+			return (element = 0);
+		} else if (channelName === 'blue' && (index % 4 === 0 || index % 4 === 1)) {
+			return (element = 0);
+		} else {
+			return element;
+		}
+		//green
+		// channelName === 'green' && index % 4 === 1 ? console.log(`${channelName}: ${index}`) : null;
+		//blue
+		// channelName === 'blue' && index % 4 === 2 ? console.log(`${channelName}: ${index}`) : null;
+		// index % 4 === 3 ? element = 1 :
+	});
+	console.log(`newArr: ${newArr}`);
+
+	/* 	for (const [i, v] of data.entries()) {
+		if (channelName === 'red') {
+			// i % 1 === 0 ||
+		}
+	} */
+	// console.info(data);
+};
+
 function getChannel(channelName, image) {
 	const copy = copyToCanvas(image);
 	const ctx = copy.ctx;
-	ctx.fillStyle = channels[channelName];
-	ctx.globalComposietOperation = 'multiply';
-	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-	ctx.globalComposietOperation = 'destination-in';
-	ctx.drawImage(image, 0, 0);
-	ctx.globalComposietOperation = 'source-over';
+
+	const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+	// console.info(imageData.data);
+	const data = imageData.data;
+	returnChannel(channelName, data);
+
+	ctx.putImageData(imageData, 0, 0);
+
+	// ctx.globalComposietOperation = 'screen';
+	// ctx.fillStyle = channels[channelName];
+	// ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	// ctx.drawImage(image, 0, 0);
+	// ctx.globalComposietOperation = 'destination-in';
+	// ctx.globalComposietOperation = 'source-over';
 	return copy;
 }
 
@@ -49,12 +124,13 @@ const RGB = seperateRGB(image);
 const recombined = createCanvas(RGB.red.width, RGB.red.height);
 const ctx = recombined.ctx;
 
-ctx.drawImage(RGB.red, -2, -2);
-ctx.globalCompositeOperation = 'lighter';
-ctx.drawImage(RGB.green, 0, 0);
-ctx.drawImage(RGB.blue, 2, 2);
-ctx.globalCompositeOperation = 'destination-in';
-ctx.drawImage(image, 0, 0);
-ctx.globalCompositeOperation = 'source-over';
+ctx.globalCompositeOperation = 'multiply';
+ctx.drawImage(RGB.red, 0, 0);
+// ctx.globalCompositeOperation = 'overlay';
+ctx.drawImage(RGB.green, 10, 10);
+// ctx.drawImage(RGB.blue, 20, 20);
+// ctx.globalCompositeOperation = 'overlay';
+// ctx.drawImage(image, 0, 0);
+// ctx.globalCompositeOperation = 'overlay';
 
 output.append(recombined);
